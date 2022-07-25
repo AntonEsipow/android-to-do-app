4b27a8b93661fd06b5f519af50018fd56d335ffb
AntonEsipow <esipow.anton99@gmail.com>
2022-07-25 22:02:11 +0300

    Добавлена CategoryEntity и Dao. Произведена миграция базы данных для поддержки новой таблицы.

--- app/src/main/java/com/bigtoapp/todo/database/AppDatabase.kt
+++ app/src/main/java/com/bigtoapp/todo/database/AppDatabase.kt
@@ -4,12 +4,16 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.bigtoapp.todo.database.dao.CategoryEntityDao
 import com.bigtoapp.todo.database.dao.NoteEntityDao
+import com.bigtoapp.todo.database.entity.CategoryEntity
 import com.bigtoapp.todo.database.entity.NoteEntity
 
 @Database(
-    entities = [NoteEntity::class],
-    version = 1
+    entities = [NoteEntity::class, CategoryEntity::class],
+    version = 2
 )
 abstract class AppDatabase: RoomDatabase() {
 
@@ -27,10 +31,20 @@ abstract class AppDatabase: RoomDatabase() {
                     AppDatabase::class.java,
                     "to-do-database"
                 )
+                .addMigrations(MIGRATION_1_2())
                 .build()
             return appDatabase!!
         }
     }
 
+    class MIGRATION_1_2: Migration(1,2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS `category_entity` " +
+                    "(`id` TEXT NOT NULL, `name` TEXT NOT NULL, `color` INTEGER NOT NULL, PRIMARY KEY(`id`))")
+        }
+
+    }
+
     abstract fun noteEntityDao(): NoteEntityDao
+    abstract fun categoryEntityDao(): CategoryEntityDao
 }
